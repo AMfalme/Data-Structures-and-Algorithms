4804fbf94bec0ea9746898e2104d0ace1bc1a45b
Mfalme Griffin <mfalmegriffin@gmail.com>
2024-01-01 20:59:23 +0300

    complete working merge sort solution.

--- two_pointer/merge_sort.py
+++ two_pointer/merge_sort.py
@@ -1,8 +1,8 @@
 arr, array2 = [45,2,34,55,645] , [4,234,56,657,9]
 
-# n = len(arr)
-# m = len(array2)
-# final_arr = [None] * (m+n)
+n = len(arr)
+m = len(array2)
+final_arr = [None] * (m+n)
 
 # start with merging two sorted arrays
 
@@ -23,5 +23,24 @@ def merge_two_sorted_arr(arr, array2, result):
     return result
 
 print(merge_two_sorted_arr([1,3,4, 8, 10], [1, 2, 6, 9, 11], [None] * 10))
+
+# create function to sort the arrays first then merge sort
+
+
+def sort_arr(S):
+    if len(S) < 2:
+        return S
+    m = len(S) // 2
+
+    SLeft = S[:m]
+    SRight = S[m:]
+    sort_arr(SLeft)
+    sort_arr(SRight)
+
+    merge_two_sorted_arr(SLeft, SRight, S)
+    return S
+
+
+print(merge_two_sorted_arr(sort_arr(arr), sort_arr(array2), final_arr))
 
 
